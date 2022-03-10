664359acf737d71505d5b318a7e920b43170928d
flashniv <flashniv@gmail.com>
2022-03-10 11:47:39 +0200

    init 1

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,13 @@
+import {Box} from "@mui/material";
+
+export default function Dashboard() {
+    return(
+        <Box
+            sx={{
+                backgroundColor:"green"
+            }}
+        >
+            text
+        </Box>
+    );
+}
